7766b0b3c5472a22af22a95570115812f842ff3d
glenn <glenn.reumers@hotmail.com>
2025-09-28 08:29:54 +0200

    refactor: remove unused authentication pages and improve code structure

--- apps/nuxt/layers/base/pages/auth/login.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import AuthLoginView from '~base/modules/auth/api/views/AuthLoginView.vue'
-</script>
-
-<template>
-  <AuthLoginView />
-</template>
--- apps/payload/eslint.config.js
+++ apps/payload/eslint.config.js
@@ -18,6 +18,7 @@ export default [
       '**/app/(payload)',
       '**/migrations',
       '**/payload-types.ts',
+      '**/.turbo',
     ],
   },
 ]
